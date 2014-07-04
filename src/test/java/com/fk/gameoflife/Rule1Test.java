5642a025a8c27b0bf98f1c44e9465f51da14dc13
Sathish <sathish316@gmail.com>
2014-07-04 16:33:30 +0530

    rename Rule1Test

--- src/test/java/com/fk/gameoflife/Rule1Test.java
+++ src/test/java/com/fk/gameoflife/Rule1Test.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class GameOfLifeRuleTest {
+public class Rule1Test {
     @Test
     public void testUnderpopulationRule(){
         World world = new World(new String[]{
