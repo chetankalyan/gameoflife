aab76aa189ec7ba31f24aa882757b8c40cd770f5
Sathish <sathish316@gmail.com>
2014-07-08 15:28:07 +0530

    removed ignore

--- src/test/java/com/fk/gameoflife/GameOfLifeTest.java
+++ src/test/java/com/fk/gameoflife/GameOfLifeTest.java
@@ -7,7 +7,6 @@ import static org.junit.Assert.assertEquals;
 
 public class GameOfLifeTest {
     @Test
-    @Ignore
     public void testOscillatorPattern(){
         World world = new World(new String[]{
                 ".....",
