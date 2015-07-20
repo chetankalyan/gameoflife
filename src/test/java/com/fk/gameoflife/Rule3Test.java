d73fca2c518ad6eb14c3234bcf1ac5925db65486
Chetan Kalyan <chetan.k@flipkart.com>
2015-07-20 20:43:27 +0530

    Add rule 3 test

---
+++ src/test/java/com/fk/gameoflife/Rule3Test.java
@@ -0,0 +1,24 @@
+package com.fk.gameoflife;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by chetan.k on 7/20/15.
+ */
+public class Rule3Test {
+    @Test
+    public void testOverpopulationRule() throws Exception {
+        World world = new World(new String[]{
+                ".*.",
+                "***",
+                ".*."});
+        // T = 0
+        assertTrue(world.isAliveAt(1,1));
+        // T = 1
+        world.tick();
+        assertFalse(world.isAliveAt(1, 1));
+    }
+}
